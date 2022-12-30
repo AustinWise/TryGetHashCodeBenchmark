1e3f11936f06292fc2ce27f96731fc97b957506d
Austin Wise <AustinWise@gmail.com>
2022-12-30 13:06:48 -0800

    benchmark

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmarks
+{
+    public class TryGetHashCode
+    {
+        private readonly List<object> mRootedObjects = new();
+        private readonly ConditionalWeakTable<object, object> mWeakTable = new();
+        private object mAnObjectInTheTable = null!;
+
+
+        [Params(1000)]
+        public int NumberOfObjects;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            for (int i = 0; i < NumberOfObjects; i++)
+            {
+                var obj = new object();
+                mRootedObjects.Add(obj);
+                mWeakTable.Add(obj, new object());
+                mAnObjectInTheTable = obj;
+            }
+        }
+
+        [Benchmark]
+        public bool TryGetNonExistentValue()
+        {
+            return mWeakTable.TryGetValue(new object(), out object _);
+        }
+
+        [Benchmark]
+        public bool TryGetExistingValue()
+        {
+            return mWeakTable.TryGetValue(mAnObjectInTheTable, out object _);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<TryGetHashCode>();
+        }
+    }
+}
